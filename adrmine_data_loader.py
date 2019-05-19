2dbf750c8ea1fdb6cc0f583286bd6cd5c9fbe6a3
Eduard Kegulskiy <ekegulsk@mail.sfsu.edu>
2019-05-19 09:04:16 -0700

    Add comments

--- adrmine_data_loader.py
+++ adrmine_data_loader.py
@@ -1,4 +1,9 @@
 """
+This file implements the ADRMineDataLoader class
+
+Package: adr_data_loader
+
+Author: Eduard Kegulskiy
 
 """
 
@@ -6,16 +11,37 @@
 from __future__ import print_function
 from __future__ import division
 
-import json
-import argparse
-import random
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer, SnowballStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
-import re, string
 
 class ADRMineDataLoader:
+    """ Class for loading ADRMine data into corresponding annotations and tweets, interconnected by Unique_ID
+        (see http://diego.asu.edu/downloads/publications/ADRMine/download_tweets.zip)
 
+               # Arguments
+                   None
+
+               # Returns
+                   A ADRMineDataLoader instance.
+
+               # Examples
+
+               ```python
+               import adrmine_data_loader
+
+               # create training and test instances
+               admine_training_data = adrmine_data_loader.ADRMineDataLoader()
+               admine_test_data = adrmine_data_loader.ADRMineDataLoader()
+
+               # load ADRMine train annotations and tweet text from ADRMine corresponding files
+               (train_annotations, train_tweets) = admine_training_data.load(train_adrmine_tweets,
+                                                                             train_adrmine_annotations)
+
+               # load ADRMine test annotations and tweet text from ADRMine corresponding files
+               (test_annotations, test_tweets) = admine_test_data.load(test_adrmine_tweets,
+                                                                       test_adrmine_annotations)
+
+
+               ```
+       """
     def __init__(self):
         self._adrmine_tweets = None
         self._adrmine_annotations = None
@@ -38,8 +64,18 @@ class ADRMineDataLoader:
                     annotation['startOffset'] = "{}".format(startOffset+diff)
                     annotation['endOffset'] = "{}".format(endOffset+diff)
 
-
     def load(self, adrmine_tweets, adrmine_annotations):
+        """loads ADRMine data into corresponding annotations and tweets, interconnected by Unique_ID
+           ADRMine data files are provided as part of http://diego.asu.edu/downloads/publications/ADRMine/download_tweets.zip
+
+        # Arguments
+            adrmine_tweets - file containing original ADRMine tweets
+            adrmine_annotations - file containing original ADRMine annotations
+
+        # Returns
+            annotations and tweets dicts
+        """
+
         print("Loading ADRMine data from {}...".format(adrmine_annotations))
 
         self._adrmine_tweets = adrmine_tweets
